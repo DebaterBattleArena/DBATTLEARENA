bbd11628dcbe0c0cd1fdcf6ef817d296089d4d7a
DebaterBattleArena <tart.earth.9c@icloud.com>
2025-09-30 14:12:12 +0700

    Create data.js

---
+++ js/data.js
@@ -0,0 +1,20 @@
+// js/data.js
+const ATHLETES_DATA = [
+    { id: 1, name: "Fabricio Andrade", country: "Brazil", weight_class: "Bantamweight MMA", record: "9-2-0", image: "img/fabricio-andrade.jpg" },
+    { id: 2, name: "Angela Lee", country: "Singapore / United States", weight_class: "Atomweight MMA", record: "11-3-0", image: "img/angela-lee.jpg" },
+    // Tambahkan data atlet lain
+];
+
+const EVENTS_DATA = [
+    { id: 101, title: "ONE Fight Night 37", date: "OCT 4 (SAT)", location: "Lumpinee Stadium, Bangkok", fights: ["Prajanchai vs. Di Bella II", "..."] },
+    // Tambahkan data acara lain
+];
+
+const RANKINGS_DATA = {
+    'Flyweight MMA': [
+        { rank: 1, athlete: 'Demetrious Johnson' },
+        { rank: 2, athlete: 'Kairat Akhmetov' },
+        // ...
+    ]
+    // Tambahkan data divisi berat lain
+};
